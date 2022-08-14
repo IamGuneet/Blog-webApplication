77f3d76382b3730bf9e157866138d228483d6237
Guneet <guneet.iam@gmail.com>
2022-08-14 20:50:19 +0530

    pushing trial 1

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
+let paras =[];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,7 +18,8 @@ app.use(express.static("public"));
 
 app.get("/",function(req,res){
   res.render("home",{
-    homeContent:homeStartingContent
+    homeContent:homeStartingContent,
+    composeParas:paras
   })
 })
 
@@ -36,14 +37,24 @@ app.get("/about",function(req,res){
 
 app.get("/compose",function(req,res){
   res.render("compose",{
+   
   })
+})
+
+app.post("/compose",function(req,res){
+   const content = {
+     title:req.body.paragraphHead,
+    para:req.body.paragraphText
+  };
+  paras.push(content);
+  // console.log(paras);
+    res.redirect("/");
 })
 
 
 
 
 
-
-app.listen(2022, function() {
+app.listen(process.env.PORT||2022, function() {
   console.log("Server started on port 2022");
 });
